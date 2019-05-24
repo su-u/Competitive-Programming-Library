451648020f02826890dc4dc58d23b94c149fe5e8
すー <aaaiiiuuu35@gmail.com>
2019-05-25 00:09:18 +0900

    Update Program.cs

--- Competitive-Programming-Library/Program.cs
+++ Competitive-Programming-Library/Program.cs
@@ -7,6 +7,23 @@ namespace CPL
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            "1".ParseToInt32();
+
+        }
+
+
+    }
+
+    static class Extensions
+    {
+        public static Int32 ParseToInt32(this string str)
+        {
+            return Int32.Parse(str);
+        }
+
+        public static Double ParseToDouble(this string str)
+        {
+            return Double.Parse(str);
         }
     }
 }
