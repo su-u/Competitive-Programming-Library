0a51d47c6bb20d64303c9bfd56d009ee93fa8a1c
すー <aaaiiiuuu35@gmail.com>
2019-05-25 21:09:19 +0900

    input

---
+++ Competitive-Programming-Library/Console.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace CPL.Input
+{
+    static class Input
+    {
+        public static T TryParse<T>(this string input)
+        {
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    return (T)converter.ConvertFromString(input);
+                }
+                else
+                {
+                    throw new InvalidCastException("");
+                }
+            }
+            catch
+            {
+                throw new InvalidCastException(typeof(T) + " is not suported.");
+            }
+        }
+
+        public static List<T> SplitTryParseToList<T>(this string input)
+        {
+            return input.Split().Select(n => n.TryParse<T>()).ToList();
+        }
+
+        public static List<T> Aa<Tsouse, T>(this Tsouse str)
+        {
+            List<T> list = new List<T>();
+            return list;
+        }
+
+        public static Int32 ParseToInt32(this String str)
+        {
+            return Int32.Parse(str);
+        }
+
+        public static Double ParseToDouble(this String str)
+        {
+            return Double.Parse(str);
+        }
+    }
+}
--- Competitive-Programming-Library/Program.cs
+++ Competitive-Programming-Library/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.ComponentModel;
+using CPL.Input;
 
 namespace CPL
 {
@@ -18,49 +19,5 @@ namespace CPL
         }
 
 
-    }
-
-    static class Extensions
-    {
-        public static T TryParse<T>(this string input)
-        {
-            try
-            {
-                var converter = TypeDescriptor.GetConverter(typeof(T));
-                if (converter != null)
-                {
-                    return (T)converter.ConvertFromString(input);
-                }
-                else
-                {
-                    throw new InvalidCastException("");
-                }
-            }
-            catch
-            {
-                throw new InvalidCastException(typeof(T) + " is not suported.");
-            }
-        }
-
-        public static List<T> SplitTryParseToList<T>(this string input)
-        {
-            return input.Split().Select(n => n.TryParse<T>()).ToList();
-        }
-
-        public static List<T> Aa<Tsouse, T>(this Tsouse str)
-        {
-            List<T> list = new List<T>();
-            return list;
-        }
-
-        public static Int32 ParseToInt32(this String str)
-        {
-            return Int32.Parse(str);
-        }
-
-        public static Double ParseToDouble(this String str)
-        {
-            return Double.Parse(str);
-        }
     }
 }
