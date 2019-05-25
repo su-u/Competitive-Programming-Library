e612390453da7c4bcadc1a64a30b471abb8d6cac
すー <aaaiiiuuu35@gmail.com>
2019-05-25 21:13:03 +0900

    add comment

--- Competitive-Programming-Library/Console.cs
+++ Competitive-Programming-Library/Console.cs
@@ -8,6 +8,12 @@ namespace CPL.Input
 {
     static class Input
     {
+        /// <summary>
+        /// 文字列を任意の型にキャストする。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値</returns>
         public static T TryParse<T>(this string input)
         {
             try
@@ -28,6 +34,12 @@ namespace CPL.Input
             }
         }
 
+        /// <summary>
+        /// 文字列を空白で区切り、任意の型にキャストしたリストに変換する。
+        /// </summary>
+        /// <typeparam name="T">変換後の型</typeparam>
+        /// <param name="input">変換する文字列</param>
+        /// <returns>キャストされた値のリスト</returns>
         public static List<T> SplitTryParseToList<T>(this string input)
         {
             return input.Split().Select(n => n.TryParse<T>()).ToList();
