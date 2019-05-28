24ea8a39e3e963194441d5c955f1ca1c352eec97
すー <aaaiiiuuu35@gmail.com>
2019-05-28 18:31:45 +0900

    improve

--- Competitive-Programming-Library/Input.cs
+++ Competitive-Programming-Library/Input.cs
@@ -8,12 +8,6 @@ namespace CPL.Input
 {
     static class Input
     {
-        /// <summary>
-        /// 文字列を任意の型にキャストする。
-        /// </summary>
-        /// <typeparam name="T">変換後の型</typeparam>
-        /// <param name="input">変換する文字列</param>
-        /// <returns>キャストされた値</returns>
         public static T TryParse<T>(this String input)
         {
             try
@@ -34,17 +28,31 @@ namespace CPL.Input
             }
         }
 
-        /// <summary>
-        /// 文字列を空白で区切り、任意の型にキャストしたリストに変換する。
-        /// </summary>
-        /// <typeparam name="T">変換後の型</typeparam>
-        /// <param name="input">変換する文字列</param>
-        /// <returns>キャストされた値のリスト</returns>
         public static List<T> SplitTryParseToList<T>(this String input)
         {
             return input.Split().Select(n => n.TryParse<T>()).ToList();
         }
 
+        public static List<T> ListSwap<T>(this List<T> list, Int32 index1, Int32 index2)
+        {
+            var t = list[index1];
+            list[index1] = list[index2];
+            list[index2] = t;
+            return list;
+        }
+
+        public static List<Tuple<T, int>> DuplicateCount<T>(this IEnumerable<T> list)
+        {
+            return list
+                .GroupBy(i => i)
+                .Where(g => g.Any())
+                .Select(g => Tuple.Create(g.Key, g.Count()))
+                .ToList();
+        }
+        public static List<Tuple<T, int>> DuplicateSort<T>(this IEnumerable<Tuple<T, int>> list)
+        {
+            return list.OrderByDescending((x) => x.Item2).ToList();
+        }
         public static List<T> ReadLineOne<T>(int n)
         {
             var list = new List<T>();
@@ -54,40 +62,9 @@ namespace CPL.Input
             }
             return list;
         }
-    }
-    static class List {
-        /// <summary>
-        /// リスト内部の指定した要素同士を交換する。
-        /// </summary>
-        /// <typeparam name="T">リストの型</typeparam>
-        /// <param name="list">交換する対象のリスト</param>
-        /// <param name="index1">交換する要素番号</param>
-        /// <param name="index2">交換する要素番号</param>
-        /// <returns></returns
-        public static List<T> ListSwap<T>(this List<T> list, Int32 index1, Int32 index2)
-        {
-            var t = list[index1];
-            list[index1] = list[index2];
-            list[index2] = t;
-            return list;
-        }
-
-        public static List<Tuple<T, int>> DuplicateCount<T>(this List<T> list)
-        {
-            return list
-                .GroupBy(i => i)
-                .Where(g => g.Count() >= 1)
-                .Select(g => Tuple.Create(g.Key, g.Count()))
-                .ToList();
-        }
-        public static List<Tuple<T, int>> DuplicateSort<T>(this List<Tuple<T, int>> list)
-        {
-            return list.OrderByDescending((x) => x.Item2).ToList();
-        }
-
         public static void PrintAll<T>(this IEnumerable<T> list)
         {
-            foreach(var i in list)
+            foreach (var i in list)
             {
                 Console.Write($"{i} ");
             }
