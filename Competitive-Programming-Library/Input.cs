70a8b556d67aea16e910a69c0300c5a8d1fd4cd8
すー <aaaiiiuuu35@gmail.com>
2019-07-01 13:04:19 +0900

    Update Input.cs

--- Competitive-Programming-Library/Input.cs
+++ Competitive-Programming-Library/Input.cs
@@ -42,14 +42,17 @@ namespace CPL.Input
                 .Select(g => Tuple.Create(g.Key, g.Count()))
                 .ToList();
         }
+
         public static List<Tuple<T, int>> DuplicateSort<T>(this IEnumerable<Tuple<T, int>> list)
         {
             return list.OrderByDescending(x => x.Item2).ToList();
         }
+
         public static List<T> ReadLineOne<T>(int n)
         {
             return Enumerable.Range(1, n).Select(i => Console.ReadLine().TryParse<T>()).ToList();
         }
+
         public static void PrintAll<T>(this IEnumerable<T> list)
         {
             foreach (var i in list)
