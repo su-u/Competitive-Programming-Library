2b1780070b1de11f5039180bf0e25deae4c96a55
すー <aaaiiiuuu35@gmail.com>
2019-07-05 09:53:44 +0900

    move

--- Competitive-Programming-Library/Input/Input.cs
+++ Competitive-Programming-Library/Input/Input.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 
 namespace CPL.Input
 {
@@ -23,7 +22,7 @@ namespace CPL.Input
 
         public static List<T> SplitTryParseToList<T>(this string input, char separator = ' ')
         {
-            return input.Split(separator).Select(n => n.TryParse<T>()).ToList();
+            return input.Split(separator).Select(n => TryParse<T>(n)).ToList();
         }
 
         public static List<T> ListSwap<T>(this List<T> list, int index1, int index2)
