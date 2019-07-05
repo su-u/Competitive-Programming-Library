8145085a5aa901559a56a6db92b7bd2b31cd8b3d
すー <aaaiiiuuu35@gmail.com>
2019-07-05 10:00:29 +0900

    Remove: Collections method

--- Competitive-Programming-Library/Input/Input.cs
+++ Competitive-Programming-Library/Input/Input.cs
@@ -32,31 +32,5 @@ namespace CPL.Input
             list[index2] = t;
             return list;
         }
-
-        public static List<Tuple<T, int>> DuplicateCount<T>(this IEnumerable<T> list)
-        {
-            return list
-                .GroupBy(i => i)
-                .Select(g => Tuple.Create(g.Key, g.Count()))
-                .ToList();
-        }
-
-        public static List<Tuple<T, int>> DuplicateSort<T>(this IEnumerable<Tuple<T, int>> list)
-        {
-            return list.OrderByDescending(x => x.Item2).ToList();
-        }
-
-        public static List<T> ReadLineOne<T>(int n)
-        {
-            return Enumerable.Range(1, n).Select(i => Console.ReadLine().TryParse<T>()).ToList();
-        }
-
-        public static void PrintAll<T>(this IEnumerable<T> list)
-        {
-            foreach (var i in list)
-            {
-                Console.Write($"{i} ");
-            }
-        }
     }
 }
